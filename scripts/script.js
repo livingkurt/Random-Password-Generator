ef9973a93c54138564876582c27a7b1cdd0de294
Kurt LaVacque <lavacquek@icloud.com>
2019-12-12 12:39:53 -0600

    Cleaned up script file

--- scripts/script.js
+++ scripts/script.js
@@ -90,31 +90,3 @@ function copy_to_clipboard() {
     alert("Copied to Clipboard\n"+ copyText.value);
   }
 
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("copy_to_clipboard_b");
-
-// var modal = document.write(copyText.value);
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
