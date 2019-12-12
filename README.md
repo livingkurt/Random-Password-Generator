27fe356e694290d7a5b89bb9dd44a53be6610660
Kurt LaVacque <lavacquek@icloud.com>
2019-12-11 18:04:06 -0600

    Added comments to js file, Added information to Readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Random-Password-Generator
+
+## Front End:
+
+The user will be prompted with questions about the password length and what characters to include or exclude from the password
+
+Then they will press the "Generate Password" button.
+
+A password will appear in the textarea to the users specifications
+
+The user can then manually copy it from the textarea or click the "Copy to Clipboard" to automatically copy it to their clipboard, where they can paste it where ever they would like.
+
+## Back End:
+
+I started out defining 4 string variables for special characters, numbers, uppercase and lowercase letters.
+
+From the data the user provides a set of if statements determines what will characters be included in the password.
+
+### When the user clicks "Generate Password":
+
+Using For loops, there will 2 rounds of randomization to create the password.
+
+Based on what the length of the password is, each character type will go through the first wave of randomization, which will create a single long string.
+
+Then there is a final round of randomization that randomly picks letters for the final password.
+
+### When the user clicks "Copy to Clipboard":
+
+The password within the textarea is selected, then copied, and pasted into the users clipboard
+
+A alert will show confirming that the password was copied to clipboard correctly
+
+
+
+### Image Below
+
+![Password Generator](images/Password_Generator.png)
+
--- script.js
+++ script.js
@@ -11,62 +11,77 @@ var numbers = "0123456789"
 var upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lower_letters = upper_letters.toLowerCase()
 
-
+//Determines whether there is a qualifying number in the password length prompt
 if (password_length_pref == null || password_length_pref < 8 || password_length_pref > 20) {
     alert("You need to choose a password length within the specificatinos to continue")
     location.reload();
 }
-else if ( special_char_pref === false && numbers_pref === false && lowercase_pref === false && uppercase_pref === false) {
+//Determines whether the user has selected at least one type of character for there password
+else if (special_char_pref === false && numbers_pref === false && lowercase_pref === false && uppercase_pref === false) {
     alert("You need to choose at least one character type")
     location.reload();
 }
 
 
+//Preiminary Round of Random
 function generate_password(a, b, c, d, e) {
+    // inialize a variable to house the initial oversized password
     var password = ""
+    //Loop through all the user specified characters randomly
     for (var i = 0; i < a; i++){
-        if (b == true) {
-            for (var i = 0; i < a; i++) {
+        if (b == true) { //If User chose special characters
+            for (var i = 0; i < a; i++) { //Loop Through the special characters list the user specified amount
+                //Choose randomly from list specified
                 var special_char_rand = special_char[Math.floor(Math.random()*special_char.length)];
+                //Add random characters to initialized variable outside of the for loop
                 password = password + special_char_rand
             }
         }
-        if (c == true) {
-            for (var i = 0; i < a; i++) {
+        if (c == true) { //If User chose numbers
+            for (var i = 0; i < a; i++) { //Loop Through the numbers list the user specified amount
+                //Choose randomly from list specified
                 var numbers_rand = numbers[Math.floor(Math.random()*numbers.length)];
+                //Add random characters to initialized variable outside of the for loop
                 password = password + numbers_rand
             }
         }
-        if (d == true) {
-            for (var i = 0; i < a; i++) {
+        if (d == true) { //If User chose uppercase letters
+            for (var i = 0; i < a; i++) { //Loop Through the uppercase list the user specified amount
+                //Choose randomly from list specified
                 var upper_letters_rand = upper_letters[Math.floor(Math.random()*upper_letters.length)];
+                //Add random characters to initialized variable outside of the for loop
                 password = password + upper_letters_rand
             }
         }
-        if (e == true) {
-            for (var i = 0; i < a; i++) {
+        if (e == true) { //If User chose lowercase letters
+            for (var i = 0; i < a; i++) { //Loop Through the lower case list the user specified amount
+                //Choose randomly from list specified
                 var lower_letters_rand = lower_letters[Math.floor(Math.random()*lower_letters.length)];
+                //Add random characters to initialized variable outside of the for loop
                 password = password + lower_letters_rand
             }
         }
     }
+    // inialize a variable to house the initial correct sized password
     var password_rand_length = ""
+    //Loop through all the user specified characters randomly a second time
     for (var i = 0; i < a; i++){
         var password_length = password[Math.floor(Math.random()*password.length)];
         password_rand_length = password_length + password_rand_length
     }
     console.log(password_rand_length)
+    //Places the password in the textarea
     document.getElementById("password_area").innerHTML = password_rand_length;
-    document.getElementsByTagName("p").innerHTML = password_rand_length;
+    // document.getElementsByTagName("p").innerHTML = password_rand_length;
 }
 
-function myFunction() {
+function copy_to_clipboard() {
     /* Get the text field */
     var copyText = document.getElementById("password_area");
   
     /* Select the text field */
     copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
   
     /* Copy the text inside the text field */
     document.execCommand("copy");
@@ -81,6 +96,8 @@ function myFunction() {
 
 // // Get the button that opens the modal
 // var btn = document.getElementById("copy_to_clipboard_b");
+
+// var modal = document.write(copyText.value);
 
 // // Get the <span> element that closes the modal
 // var span = document.getElementsByClassName("close")[0];
